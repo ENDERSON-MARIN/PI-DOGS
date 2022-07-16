3216d02aceb94157a4a24f6cc258420e249b5738
Enderson Marín <46828206+ENDERSON-MARIN@users.noreply.github.com>
2022-07-16 09:29:18 -0500

    BACKEND->CREATE MODELS DATABASE AND RUN SERVER

--- api/src/db.js
+++ api/src/db.js
@@ -30,12 +30,14 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Dog } = sequelize.models;
+const { Dog, Temperament } = sequelize.models;
 
 // Aca vendrian las relaciones
-// Product.hasMany(Reviews);
+Dog.belongsToMany(Temperament, { through: 'Dog_Temperaments' });
+Temperament.belongsToMany(Dog, { through: 'Dog_Temperaments' });
+
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+  database: sequelize,     // para importart la conexión { conn } = require('./db.js');
 };
--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,12 +1,47 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "dog",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      height_min: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      height_max: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      weight_min: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      weight_max: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      life_years: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
     },
-  });
+    {
+      timestamps: true,
+    }
+  );
 };
