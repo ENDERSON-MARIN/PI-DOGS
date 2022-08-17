31950d0cef899d9c36150c1ae2e2a4fce36a224d
Enderson Marín <46828206+ENDERSON-MARIN@users.noreply.github.com>
2022-08-17 15:22:30 -0500

    TEST->INIT ADD TEST BACKEND

--- api/tests/models/dog.spec.js
+++ api/tests/models/dog.spec.js
@@ -18,5 +18,45 @@ describe('Dog model', () => {
         Dog.create({ name: 'Pug' });
       });
     });
+    describe('height_min', () => {
+      it('should throw an error if height_min is null', (done) => {
+        Dog.create({})
+          .then(() => done(new Error('It requires a valid height_min')))
+          .catch(() => done());
+      });
+      it('should work when its a valid height_min', () => {
+        Dog.create({ height_min: 36 });
+      });
+    });
+    describe('height_max', () => {
+      it('should throw an error if height_max is null', (done) => {
+        Dog.create({})
+          .then(() => done(new Error('It requires a valid height_max')))
+          .catch(() => done());
+      });
+      it('should work when its a valid height_max', () => {
+        Dog.create({ height_max: 66 });
+      });
+    });
+    describe('weight_min', () => {
+      it('should throw an error if weight_min is null', (done) => {
+        Dog.create({})
+          .then(() => done(new Error('It requires a valid weight_min')))
+          .catch(() => done());
+      });
+      it('should work when its a valid weight_min', () => {
+        Dog.create({ weight_min: 46 });
+      });
+    });
+    describe('weight_max', () => {
+      it('should throw an error if weight_max is null', (done) => {
+        Dog.create({})
+          .then(() => done(new Error('It requires a valid weight_max')))
+          .catch(() => done());
+      });
+      it('should work when its a valid weight_max', () => {
+        Dog.create({ weight_max: 76 });
+      });
+    });
   });
 });
--- api/tests/routes/dog.spec.js
+++ api/tests/routes/dog.spec.js
@@ -4,9 +4,9 @@ const session = require('supertest-session');
 const app = require('../../src/app.js');
 const { Dog, database } = require('../../src/db.js');
 
-const agent = session(app);
+const server = session(app);
 const dog = {
-  name: 'Pug',
+    name: "Pug",
 };
 
 describe('Dog routes', () => {
@@ -14,11 +14,11 @@ describe('Dog routes', () => {
   .catch((err) => {
     console.error('Unable to connect to the database:', err);
   }));
-  beforeEach(() => Dog.sync({ force: true })
-    .then(() => Dog.get(dog)));
+  beforeEach(() => Dog.sync({ force: false })
+    .then(() => Dog.create(dog)));
   describe('GET /dogs', () => {
     it('should get 200', () =>
-      agent.get('/dogs').expect(200)
+      server.get('/dogs').expect(200)
     );
   });
 });
